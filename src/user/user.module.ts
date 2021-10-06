687bc342dae077886a4a724172a208337125755f
Jesus Marenco <jesusmarencodev@gmail.com>
2021-10-06 07:20:05 -0500

    Schema and model

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { USER } from '../common/models/models';
+import { UserSchema } from './schema/user.schema';
 
 @Module({
+  imports:[
+    MongooseModule.forFeatureAsync(
+    [ {
+        name : USER.name,
+        useFactory:()=>{
+          return UserSchema
+        }
+      }
+    ]),
+
+  ],
   controllers: [UserController],
   providers: [UserService]
 })
