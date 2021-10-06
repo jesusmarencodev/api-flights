f7fe33f740fe5058007f214a8fab0f3e7d2b7b68
Jesus Marenco <jesusmarencodev@gmail.com>
2021-10-06 09:00:56 -0500

    create and getAll passenger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { PassegerModule } from './passeger/passeger.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath:['.env.development'], isGlobal : true }),
     MongooseModule.forRoot(process.env.URI_MONGODB),
-    UserModule
+    UserModule,
+    PassegerModule
 ],
   controllers: [AppController],
   providers: [AppService],
