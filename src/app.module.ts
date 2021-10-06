a127c1064fcbb14234c182f31b768eb0e241099c
Jesus Marenco <jesusmarencodev@gmail.com>
2021-10-06 07:45:28 -0500

    create user in db

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath:['.env.development'], isGlobal : true }),
-    MongooseModule.forRoot(process.env.URI_MONGODB, { useCreateIndex :true, useFindAndModify : false }),
+    MongooseModule.forRoot(process.env.URI_MONGODB),
     UserModule
 ],
   controllers: [AppController],
